342b5cdece54fbe2f8b9fdd66d8e19e5d4706c7e
iamcopper <kangpan519@gmail.com>
2020-12-25 08:30:05 +0800

    [can] add can sample: cansend.c and canrecv.c

---
+++ socket/can/canrecv.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#define IF_NAME   "can0"
+
+/* To enable/disable the frame filter */
+#undef FRAME_FILTER
+
+int main(int argc, char *argv[])
+{
+	int sockfd, ret;
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+
+	/* CAN socket */
+	sockfd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if (sockfd < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	/* Set CAN device "can0" */
+	strcpy(ifr.ifr_name, IF_NAME);
+	ret = ioctl(sockfd, SIOCGIFINDEX, &ifr);
+	if (ret < 0) {
+		perror("ioctl");
+		return -1;
+	}
+
+	/* Bind can0 to sockfd */
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+	bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+
+#ifdef FRAME_FILTER
+	/* Add frame filter rule to get the wanted frame */
+	struct can_filter filter[1];
+	filter[0].can_id = 0x11;
+	filter[0].can_mask = C_SFF_MASK;
+	setsockopt(sockfd, SOL_CAN_RAW, CAN_RAW_FILTER, &filter, sizeof(filter));
+#endif
+
+	/* CAN frame */
+	struct can_frame frame;
+
+	int cycle = 0;
+	while(cycle++ < 5) {
+		ret = recv(sockfd, &frame, sizeof(frame), 0);
+		if (ret <= 0) {
+			perror("recv");
+			continue;
+		}
+
+		printf("[cycle=%d] ID=0x%x DLC=%d data[0]=0x%x data[1]=0x%x\n",
+					cycle, frame.can_id, frame.can_dlc, frame.data[0], frame.data[1]);
+
+		sleep(1);
+	}
+
+	close(sockfd);
+
+	return 0;
+}
---
+++ socket/can/cansend.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/ioctl.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <net/if.h>
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#define IF_NAME   "can0"
+
+int main(int argc, char *argv[])
+{
+	int sockfd, ret;
+	struct sockaddr_can addr;
+	struct ifreq ifr;
+
+	/* CAN socket */
+	sockfd = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+	if (sockfd < 0) {
+		perror("socket");
+		return -1;
+	}
+
+	/* Set CAN device "can0" */
+	strcpy(ifr.ifr_name, IF_NAME);
+	ret = ioctl(sockfd, SIOCGIFINDEX, &ifr);
+	if (ret < 0) {
+		perror("ioctl");
+		return -1;
+	}
+
+	/* Bind can0 to sockfd */
+	addr.can_family = AF_CAN;
+	addr.can_ifindex = ifr.ifr_ifindex;
+	bind(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+
+	/* Disable frame filter rule, we don't receive CAN frame */
+	setsockopt(sockfd, SOL_CAN_RAW, CAN_RAW_FILTER, NULL, 0);
+
+	/* Construct 2 frames ready to send */
+	struct can_frame frame[2];
+	frame[0].can_id = 0x12;                    /* CAN ID: Standard frame */
+	frame[0].can_dlc = 2;                       /* Data length */
+	frame[0].data[0] = 0x01;                    /* Data content */
+	frame[0].data[1] = 0xAB;
+	frame[1].can_id = CAN_EFF_FLAG | 0x12;     /* CAN ID: Extend frame */
+	frame[1].can_dlc = 2;                       /* Data length */
+	frame[1].data[0] = 0x02;                    /* Data content */
+	frame[1].data[1] = 0xCD;                    /* Data content */
+
+	int cycle = 0;
+	while(cycle++ < 5) {
+		/* Only one frame can be sent at a time */
+		ret = send(sockfd, &frame[0], sizeof(frame[0]), 0);
+		if (ret <= 0) {
+			perror("send frame 0");
+			break;
+		}
+		printf("[cycle=%d] send Standard Frame 0 success.\n", cycle);
+		sleep(1);
+
+		ret = send(sockfd, &frame[1], sizeof(frame[1]), 0);
+		if (ret <= 0) {
+			perror("send frame 1");
+			break;
+		}
+		printf("[cycle=%d] send Extend Frame 1 success.\n", cycle);
+		sleep(1);
+	}
+
+	close(sockfd);
+
+	return 0;
+}
