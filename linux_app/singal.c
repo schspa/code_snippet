e8cd8ef1d6dc4fb19607d31992361e6b34019f41
iamcopper <kangpan519@gmail.com>
2021-07-06 10:02:00 +0800

    [linux_app] update signal.c

--- linux_app/singal.c
+++ linux_app/singal.c
@@ -5,7 +5,9 @@
 
 void fun(int sig)
 {
-	if(sig == SIGUSR1) {
+	if(sig == SIGQUIT) {
+		printf("Received SIGQUIT signal.\n");
+	} else if(sig == SIGUSR1) {
 		printf("Received SIGUSR1 signal.\n");
 	} else if(sig == SIGUSR2) {
 		printf("Received SIGUSR2 signal.\n");
@@ -14,8 +16,11 @@ void fun(int sig)
 
 int main(int argc, const char *argv[])
 {
-	signal(SIGUSR1, fun);
-	signal(SIGUSR2, fun);
+	signal(SIGINT, SIG_IGN);  /* kill  -2 xxx, Ctl + C, Ignore */
+	signal(SIGTERM, SIG_DFL); /* kill -15 xxx, kill xxx, default behavior */
+	signal(SIGQUIT, fun);     /* kill  -3 xxx */
+	signal(SIGUSR1, fun);     /* kill -10 xxx */
+	signal(SIGUSR2, fun);     /* kill -12 xxx */
 
 	printf("Main start.\n");
 	while (1) {
