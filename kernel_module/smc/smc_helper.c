f48a5c23383cc20e57b8e8e1596b8d471916215e
schspa <schspa@gmail.com>
2021-12-03 11:41:26 +0800

    smc-helper: add smc helper module to build.
    Signed-off-by: schspa <schspa@gmail.com>

---
+++ kernel_module/smc/smc_helper.c
@@ -0,0 +1,203 @@
+/*
+ * smc_helper.c --- SMC helper module to perform smc invocation
+ *
+ * Copyright (C) 2021, Schspa, all rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/cdev.h>
+#include <linux/slab.h>
+#include <linux/fs.h>
+#include <linux/list.h>
+#include <linux/mutex.h>
+#include <linux/uaccess.h>
+#include <linux/arm-smccc.h>
+
+#define SMC_HELPER_MAJOR 0
+#define SMC_HELPER_NAME  "smc-helper"
+
+int smc_helper_major = 0;
+int smc_helper_minor = 0;
+
+struct smc_cmd_buf {
+	struct list_head node;
+	/* arm smccc use a0 - a7 for input */
+	unsigned long cmd_buf[8];
+	/* arm smccc use a0 - a3 for output */
+	unsigned long result_buf[4];
+};
+
+struct smc_helper_dev_t {
+	struct cdev cdev;
+	struct mutex lock;
+	struct list_head cmdb_head;
+};
+
+static int smc_helper_open(struct inode *inode, struct file *filp)
+{
+	struct smc_helper_dev_t *smc_helper_dev;
+
+	smc_helper_dev = container_of(inode->i_cdev, struct smc_helper_dev_t, cdev);
+
+	filp->private_data = smc_helper_dev;
+
+	return 0;
+}
+
+static int smc_helper_close(struct inode *inode, struct file *filp)
+{
+	return 0;
+}
+
+static ssize_t
+smc_read(struct file *file, char __user *buf, size_t count, loff_t *ppos)
+{
+	struct smc_helper_dev_t *bd = file->private_data;
+	ssize_t bytes_read = 0;
+	struct smc_cmd_buf *cmdb, *tmp;
+
+	mutex_lock(&bd->lock);
+	list_for_each_entry_safe(cmdb, tmp, &bd->cmdb_head, node) {
+		if (count < bytes_read + sizeof(cmdb->result_buf))
+			break;
+
+		if (copy_to_user(buf, cmdb->result_buf,
+					sizeof(cmdb->result_buf))) {
+			mutex_unlock(&bd->lock);
+			return -EFAULT;
+		}
+
+		bytes_read += sizeof(cmdb->result_buf);
+		list_del(&cmdb->node);
+	}
+	mutex_unlock(&bd->lock);
+
+	return bytes_read;
+}
+
+static ssize_t smc_write(struct file *file, const char __user *buf,
+                         size_t count, loff_t *ppos)
+{
+	struct smc_helper_dev_t *bd = file->private_data;
+	struct arm_smccc_res res;
+	struct smc_cmd_buf *cmdb = NULL;
+
+	if (count != sizeof(cmdb->cmd_buf)) {
+		pr_err("count %zd is invalid, must be %zd\n", count, sizeof(cmdb->cmd_buf));
+		return -EINVAL;
+	}
+
+	cmdb = kzalloc(sizeof(*cmdb), GFP_KERNEL);
+	if (!cmdb) {
+		return -ENOMEM;
+	}
+
+	INIT_LIST_HEAD(&cmdb->node);
+
+	if (copy_from_user(cmdb->cmd_buf, buf, count))
+		return -EFAULT;
+
+	arm_smccc_smc(cmdb->cmd_buf[0], cmdb->cmd_buf[1], cmdb->cmd_buf[2],
+		cmdb->cmd_buf[3], cmdb->cmd_buf[4], cmdb->cmd_buf[5],
+		cmdb->cmd_buf[6], cmdb->cmd_buf[7], &res);
+
+	cmdb->result_buf[0] = res.a0;
+	cmdb->result_buf[1] = res.a1;
+	cmdb->result_buf[2] = res.a2;
+	cmdb->result_buf[3] = res.a3;
+
+	mutex_lock(&bd->lock);
+	list_add_tail(&cmdb->node, &bd->cmdb_head);
+	mutex_unlock(&bd->lock);
+
+	return count;
+}
+
+static const struct file_operations smc_helper_fops = {
+	.owner = THIS_MODULE,
+	.open = smc_helper_open,
+	.release = smc_helper_close,
+	.read = smc_read,
+	.write = smc_write,
+};
+
+struct smc_helper_dev_t *smc_helper_dev = NULL;
+
+static int __init smc_helper_drv_init(void)
+{
+	int ret;
+	dev_t devno = 0;
+
+	ret = alloc_chrdev_region(&devno, smc_helper_minor, 1, SMC_HELPER_NAME);
+	if (ret < 0) {
+		pr_err("alloc chrdev regin faile with status %d\n", ret);
+		return ret;
+	}
+
+	smc_helper_major = MAJOR(devno);
+	smc_helper_minor = MINOR(devno);
+
+	smc_helper_dev = kzalloc(sizeof(*smc_helper_dev), GFP_KERNEL);
+	if (!smc_helper_dev) {
+		ret = -ENOMEM;
+		goto FAIL;
+	}
+
+	mutex_init(&smc_helper_dev->lock);
+	INIT_LIST_HEAD(&smc_helper_dev->cmdb_head);
+
+	cdev_init(&smc_helper_dev->cdev, &smc_helper_fops);
+	smc_helper_dev->cdev.owner = THIS_MODULE;
+	ret = cdev_add(&smc_helper_dev->cdev, devno, 1);
+	if (ret) {
+		pr_err("Error %d adding %s\n", ret, SMC_HELPER_NAME);
+	}
+
+	pr_info("%s init ok, major=%d, minor=%d\n",
+		SMC_HELPER_NAME, smc_helper_major, smc_helper_minor);
+
+	return 0;
+
+FAIL:
+	unregister_chrdev_region(devno, 1);
+	return ret;
+}
+
+static void __exit smc_helper_drv_exit(void)
+{
+	dev_t devno = MKDEV(smc_helper_major, smc_helper_minor);
+
+	if (smc_helper_dev) {
+		cdev_del(&smc_helper_dev->cdev);
+		kfree(smc_helper_dev);
+	}
+
+	unregister_chrdev_region(devno, 1);
+
+	pr_info("%s exit ok, major=%d, minor=%d\n",
+		SMC_HELPER_NAME, smc_helper_major, smc_helper_minor);
+}
+
+module_init(smc_helper_drv_init);
+module_exit(smc_helper_drv_exit);
+MODULE_AUTHOR("Schspa <schspa@gmail.com>");
+MODULE_LICENSE("GPL v2");
---
+++ kernel_module/smc/utils/smc.c
@@ -0,0 +1,148 @@
+/*
+ * smc.c --- smc command to perform smc invocation
+ *
+ * Copyright (C) 2021, Schspa, all rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <errno.h>
+
+#define SMC_HELPER_PATH "/dev/smc-helper"
+#define SMC_CMD_LENGTH 8
+#define SMC_RES_LENGTH 4
+
+/**
+ * xtou64
+ * Take a hex string and convert it to a 64bit number (max 16 hex digits).
+ * The string must only contain digits and valid hex characters.
+ */
+uint64_t xtou64(const char *str)
+{
+	uint64_t res = 0;
+	char c;
+
+	while ((c = *str++)) {
+		char v = (c & 0xF) + (c >> 6) | ((c >> 3) & 0x8);
+		res = (res << 4) | (uint64_t) v;
+	}
+
+	return res;
+}
+
+int main (int argc, char **argv)
+{
+	int aflag = 0;
+	int bflag = 0;
+	char *smc_helper_path = SMC_HELPER_PATH;
+	int index;
+	bool use_hex = true;
+        uint64_t cmd[SMC_CMD_LENGTH] = { 0x0 };
+        uint64_t res[SMC_RES_LENGTH] = { 0x0 };
+	int c;
+	FILE *smc_f;
+	size_t count;
+	int ret;
+
+        opterr = 0;
+
+	while ((c = getopt (argc, argv, "hDd:")) != -1)
+		switch (c)
+		{
+		case 'h':
+			fprintf(stdout, "Usage %s a0 a1 a2 a3 ...\n", argv[0]);
+			return 0;
+			break;
+		case 'D':
+			use_hex = false;
+			break;
+		case 'd':
+			smc_helper_path = optarg;
+			break;
+		case '?':
+			if (optopt == 'd')
+				fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+			else if (isprint (optopt))
+				fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+			else
+				fprintf(stderr,
+					"Unknown option character `\\x%x'.\n",
+					optopt);
+			return 1;
+		default:
+			abort ();
+		}
+
+	if (optind == argc) {
+		fprintf(stderr, "SMC command argument required\n");
+		return 2;
+	}
+
+	for (index = optind; index < argc; index++) {
+		if (use_hex) {
+			cmd[index - optind] = xtou64(argv[index]);
+		} else {
+			cmd[index - optind] = atol(argv[index]);
+		}
+		// printf("cmd[%d]: 0x%lx\n", index - optind, cmd[index - optind]);
+	}
+
+	smc_f = fopen(smc_helper_path, "r+");
+	if (!smc_f) {
+		perror(smc_helper_path);
+		return errno;
+	}
+
+	count = fwrite(cmd, sizeof(cmd), 1, smc_f);
+	if (count < 1) {
+		fprintf(stderr, "Faile to write command\n");
+		fclose(smc_f);
+		ret = -EIO;
+		goto out;
+	}
+
+	count = fread(res, sizeof(res), 1, smc_f);
+	if (count < 1) {
+		fprintf(stderr, "Faile to read result\n");
+		fclose(smc_f);
+		ret = -EIO;
+		goto out;
+	}
+	for (index = 0; index < sizeof(res) / sizeof(res[0]); index++) {
+		if (index != 0)
+			fprintf(stdout, " ");
+		if (use_hex) {
+			fprintf(stdout, "0x%lx", res[index]);
+		} else {
+			fprintf(stdout, "0x%lu", res[index]);
+		}
+	}
+	fprintf(stdout, "\n");
+
+out:
+	fclose(smc_f);
+
+	return 0;
+}
